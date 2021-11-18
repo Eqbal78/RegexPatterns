437dfe2f43831ebab93c811916dd0999e8d0e2c2
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-11-18 20:16:06 +0530

    Start with alphabet using Regex Pattern

---
+++ RegexPattern/RegexPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RegexPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            RegexPttrn.RegexPattern();
+        }
+    }
+}
---
+++ RegexPattern/RegexPattern/Regex.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace RegexPattern
+{
+    class RegexPttrn
+    {
+     
+        public static void RegexPattern()
+        {
+
+            //Regex regex = new Regex(@"\b[M]\w+");
+            string pattern = @"\b[M]\w+";
+            Regex regex = new Regex(pattern);
+
+            string authors = "Mahesh Chand, Raj Kumar, Mike Gold, Allen O'Neill, Marshal Troll";
+
+           
+            MatchCollection matchedAuthors = regex.Matches(authors);
+             
+            for (int i = 0; i < matchedAuthors.Count; i++)
+                Console.WriteLine(matchedAuthors[i].Value);
+
+        }
+    }
+}
